933be7a03da854e300b0c1b46d0cf257eec1f20d
River <swimmingr@gmail.com>
2021-12-15 22:30:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [doThing,setDoThing]=useState("");
+  const [doList,setDoList]=useState([]);
+  const [doCheck,setDoCheck]=useState(false);
+  const onclick = (e) => {
+    e.preventDefault();
+    if (doThing === "") {
+        return;
+    }
+    setDoThing("");
+    setDoList(currentArray => [
+        doThing, ...currentArray
+    ]);
+}
+  const onchange = (e) => {
+    setDoThing(e.target.value)
+}
+  const doneIt = (value) => {
+    setDoCheck(true);
+    doCheck
+        ? setDoCheck(false)
+        : setDoCheck(true);
+    // console.log(value);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className="App">
+    <h1 className="title">BucketList</h1>
+    <input placeholder="Do Things" onChange={onchange} value={doThing}></input>
+    <button onClick={onclick}>+</button>
+    <hr/>
+    <h2>Added List</h2>
+    <p>List:{doList.map((doList,index)=><li key={index}>{doList}
+            <button onClick={doneIt}>{doCheck?"done":"not yet"}</button>
+        </li>)}</p>
+</div>
   );
 }
 
